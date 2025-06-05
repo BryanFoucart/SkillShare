86a5788669af7f6ea4f5277e2151f5a74ca90851
bryan.foucart97@gmail.com <bryan.foucart97@gmail.com>
2025-06-05 16:08:00 +0200

    Update

--- backEnd/public/index.php
+++ backEnd/public/index.php
@@ -1,12 +1,20 @@
 <?php
 
+use Exception;
 use App\core\Router;
 
 require_once __DIR__ . '/../bootstrap.php';
 
-$router = new Router();
+try {
+    $router = new Router();
 
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$method = $_SERVER['REQUEST_METHOD'];
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $method = $_SERVER['REQUEST_METHOD'];
 
-$router->dispatch($uri, $method);
+    $router->dispatch($uri, $method);
+} catch (Exception $e) {
+    $json = json_encode([
+        'error' => 'Une erreur est survenue',
+        'message' => $e->getMessage()
+    ]);
+}
