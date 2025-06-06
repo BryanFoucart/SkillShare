95d87c3c57295c3f540554358abd8b5f6cf77de6
bryan.foucart97@gmail.com <bryan.foucart97@gmail.com>
2025-06-06 09:54:30 +0200

    Update

--- backEnd/public/index.php
+++ backEnd/public/index.php
@@ -1,13 +1,13 @@
 <?php
 
-use Exception;
 use App\core\Router;
+use App\core\Database;
 
 require_once __DIR__ . '/../bootstrap.php';
 
 try {
     $router = new Router();
-
+    $db = Database::getConnexion();
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $method = $_SERVER['REQUEST_METHOD'];
 
